ba1b4adbe8fc2484f46426c655535db7b98782e8
anamsayyed016 <anamsayyed58@gmail.com>
2025-01-15 15:42:30 +0530

    pyday3

---
+++ python1/3rdday.py
@@ -0,0 +1,39 @@
+# membership--its use for collection is part of the string and or not.collectiom of character
+# str1='Anam'
+# print('n' in str1)
+# print('n' not in str1)
+# print ('N' in str1)
+# print('N' not in str1)
+
+# indentity opreator
+
+# is,is not
+
+x=10     
+y=20
+# false>>>
+print(x is y)
+
+x=10
+print(id(x))
+y=10
+print(id(y))
+# 140727416784072
+# 140727416784072
+# true
+print( x is y)
+
+x=[10]
+print(id(x))
+y=[10]
+print(id(y))
+# 1401332916544
+# 1401333065024
+print(x is y)  
+# false
+print(x==y)
+# true
+
+#Bitwise opreator-&,|,~,^,<<,>>
+# number system-collection of digits
+
